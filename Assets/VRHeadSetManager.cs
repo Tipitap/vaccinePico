ec0bad934edaafe2d6c4d7516da1aa7e2cfb63aa
Agustin Pontura <pontura@gmail.com>
2021-03-16 13:33:38 -0300

    picoVR

---
+++ Assets/VRHeadSetManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHeadSetManager : MonoBehaviour
+{
+    //public Pvr_UnitySDKManager PicoVRHeadset;
+    //public OVRCameraRig OculusVRHeadset;
+
+    void Start() 
+    {
+        //if(PersistentData.Instance.headsetType == PersistentData.HeadsetTypes.oculus_go)
+        //{
+        //    PicoVRHeadset.gameObject.SetActive(false);
+        //   // OculusVRHeadset.gameObject.SetActive(true);
+        //}
+        //else
+        //{
+        //    PicoVRHeadset.gameObject.SetActive(true);
+        //   // OculusVRHeadset.gameObject.SetActive(false);
+        //}
+    }
+}
